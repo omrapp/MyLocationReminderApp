1933822750f11cb97f9e4c0e294376a722320d4e
omrapp <omar.ayed.7@gmail.com>
2025-11-03 08:47:06 +0200

    initial commit

--- android/app/src/main/java/com/mylocationreminderapp/MainActivity.kt
+++ android/app/src/main/java/com/mylocationreminderapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.mylocationreminderapp
 
+import android.os.Bundle; // add this
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen;
+
 
 class MainActivity : ReactActivity() {
 
@@ -19,4 +22,9 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        SplashScreen.show(this);  // add this
+        super.onCreate(null)
+    }
 }
